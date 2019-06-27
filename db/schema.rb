25766197ec9c0e0d0ccf91ffe1235913dfd3919f
sugioka hidekazu <h.sugioka0708@gmail.com>
2019-06-27 12:19:32 +0900

    データベース変更（マイグレーション）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_143718) do
+ActiveRecord::Schema.define(version: 2019_06_25_132245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_06_16_143718) do
     t.string "last_name", null: false
     t.integer "tel", null: false
     t.text "address", null: false
-    t.boolean "admin", default: false
+    t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
