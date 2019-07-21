609bacb26efdc81670a9ad45ac7b89cac6a35838
sugioka hidekazu <h.sugioka0708@gmail.com>
2019-07-21 10:31:32 +0900

    画像登録の対応完了

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
-    enum category:{unselected:0,hakama:1,furisode:2,houmongi:3}
-    enum color:{unselected1:0,red:1,blue:2,brack:3,white:4,yellow:5,}
-    enum scene:{unselected2:0,ceremony:1,wedding:2,party:3}
-    validates :name, presence: true
+  has_one_attached :image
+  enum category:{unselected:0,hakama:1,furisode:2,houmongi:3}
+  enum color:{unselected1:0,red:1,blue:2,brack:3,white:4,yellow:5,}
+  enum scene:{unselected2:0,ceremony:1,wedding:2,party:3}
+  validates :name, presence: true
 end
