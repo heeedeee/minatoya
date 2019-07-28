969406e020faa5781c29966bef46e69f61d8123c
sugioka hidekazu <h.sugioka0708@gmail.com>
2019-07-28 23:05:23 +0900

    ユーザーコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,3 +11,5 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+  resources :kimonos
